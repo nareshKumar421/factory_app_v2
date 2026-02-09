61f02b254808c1ab8cb6e8e9e293d286c4baee8e
nareshKumar421 <dev01@jivo.in>
2026-02-09 17:16:05 +0530

    feat: Add UserSerializer and UserListView for user management

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import DepartmentListView, LoginView
-from .views import ChangePasswordView, MeView, CustomTokenRefreshView
+from .views import ChangePasswordView, MeView, CustomTokenRefreshView,UserListView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("change-password/", ChangePasswordView.as_view()),
     path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("me/", MeView.as_view(), name="me"),
+    path('users/', UserListView.as_view(), name='user-list'),
 
     path("departments/", DepartmentListView.as_view(), name="department-list"),
 ]
